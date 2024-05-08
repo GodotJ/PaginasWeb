e78ffc288c9cb4dd8b89575b75f5c3f2c8a1674a
GodotJ <159208842+GodotJ@users.noreply.github.com>
2024-05-07 18:19:35 -0700

    Se agregaron Imagenes de turismo y comidas

--- php/FinalRegistro.php
+++ php/FinalRegistro.php
@@ -1,22 +1,31 @@
 <?php
 
-   $conexion= mysqli_connect("localhost", "root", "","pfinal");
+$servidor = "localhost";
+$usuario = "root";
+$clave = "";
+$bd = "pfinal";
+   $conexion= mysqli_connect("$servidor", "$usuario", "$clave", "$bd");
 
-   if(isset($_POST['registro'])){ 
-      $nombre = strtoupper($_POST['nombre']);
-      $apellido = strtoupper($_POST['apellido']);
-      $edad  = $_POST['edad'];
-      $ciudad = strtoupper($_POST['ciudad']);
-      $celular = $_POST['celular'];
-      $transporte = strtoupper($_POST['transporte']);
-      $camisa = strtoupper($_POST['camisa']);
-      $talla = strtoupper($_POST['talla']);
-      $comentarios = $_POST['comentarios'];
-      $sexo  = strtoupper($_POST['sexo']);
+?>
 
-         $sql = "INSERT INTO registros VALUES('','$nombre', '$apellido', '$edad','$ciudad','$celular','$transporte','$camisa','$talla','$comentarios','$sexo')";
+<?php
 
-         $ejecutar = mysqli_query ($conexion,$sql)
-   }
+if(isset($_POST['registro'])){ 
+   $nombre = strtoupper($_POST['nombre']);
+   $apellido = strtoupper($_POST['apellido']);
+   $edad  = $_POST['edad'];
+   $ciudad = strtoupper($_POST['ciudad']);
+   $celular = $_POST['celular'];
+   $transporte = strtoupper($_POST['transporte']);
+   $camisa = strtoupper($_POST['camisa']);
+   $talla = strtoupper($_POST['talla']);
+   $comentarios = $_POST['comentarios'];
+   $sexo  = strtoupper($_POST['sexo']);
+
+      $sql = "INSERT INTO registros VALUES('','$nombre', '$apellido', '$edad','$ciudad','$celular','$transporte','$camisa','$talla','$comentarios','$sexo')";
+
+      $ejecutar = mysqli_query ($conexion,$sql);
+}
+
 ?>
 
